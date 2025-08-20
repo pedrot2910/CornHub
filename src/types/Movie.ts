287a1f9a83e5cb57df6cedef00771ad90118efa9
pedrinScripts <pedro.neto2709@alu.ufc.br>
2025-08-20 17:39:49 -0300

    Front end estrutura

---
+++ src/types/Movie.ts
@@ -0,0 +1,10 @@
+export interface Movie {
+  id: number
+  title: string
+  poster_path: string
+  backdrop_path: string
+  overview: string
+  release_date: string
+  vote_average: number
+  genre_ids: number[]
+}
